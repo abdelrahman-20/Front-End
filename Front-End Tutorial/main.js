e0ca0b78e1dd0ad4c4693f21416cc3abf79f279b
abdelrahman-20 <abdelrahman.salem.1002@gmail.com>
2025-08-25 16:56:05 +0300

    Landing Section

--- Front-End Tutorial/main.js
+++ Front-End Tutorial/main.js
@@ -1,26 +1,11 @@
-// JS Lab-3 Assignment //
-// ------------------- //
+var list = document.querySelector("ul");
 
-function filterAndSortNumbers(mixedArray) {
-  mixedArray = mixedArray.filter((item) =>
-    Number.isFinite(item)
-  );
-  mixedArray.sort();
-  return mixedArray;
+var count = 0;
+var child = list.firstElementChild;
+
+while (child) {
+  child = child.nextElementSibling;
+  count++;
 }
 
-// Ai Generated Examples
-
-console.log(
-  filterAndSortNumbers([3, "hello", 1, 7, null, 2])
-);
-console.log(
-  filterAndSortNumbers([10, "5", 5, undefined, 0, -2])
-);
-
-console.log(
-  filterAndSortNumbers(["a", {}, [], 4.5, 2.2, NaN])
-);
-console.log(
-  filterAndSortNumbers([Infinity, -Infinity, 8, 3, 1])
-);
+console.log(count);
